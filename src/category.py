353718a1aa3e83b3f36f5091e9bc08554502c532
HapkoM <Ama@gmail.com>
2026-03-18 17:06:47 +0800

    Добавлены магичские методы: __str__ в Product и Category, __add__ в Product, CategoryIterator

--- src/category.py
+++ src/category.py
@@ -8,14 +8,37 @@ class Category:
     category_count = 0
     product_count = 0
 
-    def __init__(self, name: str, description: str, products: list[Product]):
+    def __init__(self, name: str, description: str, products: list[Product] = None):
         """Инициализация категории"""
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products if products else []
 
         # Увеличиваем счетчик категорий при создании объекта
         Category.category_count += 1
 
         # Увеличиваем счетчик товаров на длину списка продуктов
-        Category.product_count += len(self.products)
+        Category.product_count += len(self.__products)
+
+    def add_product(self, product: Product):
+        """Метод для добавления продукта в категорию"""
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        """Геттер для списка продуктов в отформатированном виде"""
+        products_str = ""
+        for product in self.__products:
+            products_str += str(product) + "\n"  # Используем __str__ продукта
+        return products_str
+
+    @property
+    def products_list(self):
+        """Геттер для получения списка продуктов (для тестов)"""
+        return self.__products
+
+    def __str__(self):
+        """Строковое представление категории"""
+        total_quantity = sum(product.quantity for product in self.__products)
+        return f"{self.name}, количество продуктов: {total_quantity} шт."
